43f55ac21ffbd0178a4bdca9c6981684bdf81a86
MohamedAliFaidi <dalyyy3259@protonmail.com>
2023-11-03 12:13:21 +0100

    update filter to be responsive

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
               className="px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300"
             >
               <i className="text-gray-400 w-5 fa fa-shopping-cart"></i>
-              <span className="hidden lg:inline ml-1">
+              <span className="inline ml-1">
                 Cart (<b>{cart.cartItems?.length}</b>)
               </span>
             </Link>
@@ -41,7 +41,7 @@ const Header = () => {
               className="px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300"
             >
               <i className="text-gray-400 w-5 fa fa-user"></i>
-              <span className="hidden lg:inline ml-1">Sign in</span>
+              <span className="inline ml-1">Sign in</span>
             </Link>
             <Link href="/me">
               <div className="flex items-center mb-4 space-x-3 mt-4 cursor-pointer">
