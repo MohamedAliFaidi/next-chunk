2c8d45ceecd965cda3188c9c460af1970a5840a2
MohamedAliFaidi <dalyyy3259@protonmail.com>
2023-12-27 11:22:02 +0100

    adding trycatch block to update profiile route

--- app/api/auth/me/update/route.js
+++ app/api/auth/me/update/route.js
@@ -3,7 +3,6 @@ import User from "../../../../../helper/user.model";
 import { v2 as cloudinary } from "cloudinary";
 import dbConnet from "../../../../../helper/db";
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -12,46 +11,56 @@ cloudinary.config({
 });
 
 export async function POST(req, res) {
-  const formData = await req.formData();
-  const updated = {
-    name: formData.get("name"),
-    email: formData.get("email"),
-  };
-  if (typeof formData.get("image") === "object") {
-    const file = formData.get("image");
-    const arrayBuffer = await file.arrayBuffer();
-    const buffer = new Uint8Array(arrayBuffer);
-    await new Promise((resolve, reject) => {
-      cloudinary.uploader
-        .upload_stream({ folder: "avatars" }, function (error, result) {
-          if (error) {
-            console.log(error);
-            reject(error);
-            return;
-          }
-          resolve(result);
-          updated.avatar = {
-            url: result.secure_url,
-            public_id: result.public_id,
-          };
-          console.log(result, "updated");
-        })
-        .end(buffer);
-    });
-  }
-  await dbConnet()
-  const user = await User.findOneAndUpdate(
-    {
+  try {
+    const formData = await req.formData();
+    const updated = {
+      name: formData.get("name"),
       email: formData.get("email"),
-    },
-    updated
-  );
-
-  return NextResponse.json(
-    { message: "succeed", user },
-
-    {
-      status: 200,
+    };
+    if (typeof formData.get("image") === "object") {
+      const file = formData.get("image");
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = new Uint8Array(arrayBuffer);
+      await new Promise((resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream({ folder: "avatars" }, function (error, result) {
+            if (error) {
+              console.log(error);
+              reject(error);
+              return;
+            }
+            resolve(result);
+            updated.avatar = {
+              url: result.secure_url,
+              public_id: result.public_id,
+            };
+            console.log(result, "updated");
+          })
+          .end(buffer);
+      });
     }
-  );
+    await dbConnet();
+    const user = await User.findOneAndUpdate(
+      {
+        email: formData.get("email"),
+      },
+      updated
+    );
+
+    return NextResponse.json(
+      { message: "succeed", user },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+
+    return NextResponse.json(
+      { message: "Error", err },
+      {
+        status: 500,
+      }
+    );
+  }
 }
